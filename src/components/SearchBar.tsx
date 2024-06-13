310ad0cd15ed367510aa41358fcd84c3ba58a70a
Chi Viet <chiviet296@gmail.com>
2024-06-13 09:47:42 +0700

    Search filter

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { Form, FormControl, FormField, FormItem } from './ui/form';
 import { Search } from 'lucide-react';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
+import { useEffect } from 'react';
 
 const formSchema = z.object({
 	searchQuery: z.string({
@@ -18,12 +19,20 @@ type Props = {
 	onSubmit: (formData: SearchForm) => void;
 	placeHolder: string;
 	onReset?: () => void;
+	searchQuery: string;
 };
 
-const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
+const SearchBar = ({ onSubmit, onReset, placeHolder, searchQuery }: Props) => {
 	const form = useForm<SearchForm>({
 		resolver: zodResolver(formSchema),
+		defaultValues: {
+			searchQuery,
+		},
 	});
+
+	useEffect(() => {
+		form.reset({ searchQuery });
+	}, [form, searchQuery]);
 
 	const handleReset = () => {
 		form.reset({
@@ -39,7 +48,7 @@ const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
 		<Form {...form}>
 			<form
 				onSubmit={form.handleSubmit(onSubmit)}
-				className={`flex items-center flex-1 gap-3 justify-center flex-row border-2 rounded-full p-3 mx-5 ${form.formState.errors.searchQuery && 'border-red-500'}`}
+				className={`flex items-center flex-1 gap-3 justify-center flex-row border-2 rounded-full p-3 ${form.formState.errors.searchQuery && 'border-red-500'}`}
 			>
 				<Search
 					strokeWidth={2.5}
@@ -61,17 +70,14 @@ const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
 						</FormItem>
 					)}
 				/>
-				{form.formState.isDirty && (
-					<Button
-						onClick={handleReset}
-						type="button"
-						variant="outline"
-						className="rounded-full"
-					>
-						Clear
-					</Button>
-				)}
-
+				<Button
+					onClick={handleReset}
+					type="button"
+					variant="outline"
+					className="rounded-full"
+				>
+					Reset
+				</Button>
 				<Button type="submit" className="rounded-full bg-orange-500">
 					Search
 				</Button>
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,34 @@
 import { useSearchRestaurants } from '@/api/RestaurantApi';
+import SearchBar, { SearchForm } from '@/components/SearchBar';
 import SearchResultCard from '@/components/SearchResultCard';
 import SearchResultInfo from '@/components/SearchResultInfo';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+export type SearchState = {
+	searchQuery: string;
+};
 
 export default function SearchPage() {
 	const { city } = useParams();
-	const { results, isLoading } = useSearchRestaurants(city);
+	const [searchState, setSearchState] = useState<SearchState>({
+		searchQuery: '',
+	});
+	const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+	const setSearchQuery = (searchFormData: SearchForm) => {
+		setSearchState((prevState) => ({
+			...prevState,
+			searchQuery: searchFormData.searchQuery,
+		}));
+	};
+
+	const resetSearch = () => {
+		setSearchState((prevState) => ({
+			...prevState,
+			searchQuery: '',
+		}));
+	};
 
 	if (isLoading) {
 		return <span>Loading...</span>;
@@ -19,6 +42,12 @@ export default function SearchPage() {
 		<div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
 			<div id="cuisines-list">insert cuisines here 😃</div>
 			<div id="main-content" className="flex flex-col gap-5">
+				<SearchBar
+					searchQuery={searchState.searchQuery}
+					onSubmit={setSearchQuery}
+					placeHolder="Search by Cuisine of Restaurant Name"
+					onReset={resetSearch}
+				/>
 				<SearchResultInfo total={results.data.length} city={city} />
 				{results.data.map((restaurant) => (
 					<SearchResultCard key={restaurant._id} restaurant={restaurant} />
